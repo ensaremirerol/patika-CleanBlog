96225e110af4367e946801983c94b91ad196f367
Ensar Emir EROL <ensaremir.erol99@gmail.com>
2021-05-26 23:56:39 +0300

    Clean Blog Projesi 2. Bölüm

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const ejs = require('ejs');
 const port = 3000;
 const app = express();
 
-const data = { id: 1, title: "Blog title", description: "Blog description" }
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.send(data);
+    res.render("index");
+})
+.get("/about", (req, res) => {
+    res.render("about");
+})
+.get("/add_post", (req, res) => {
+    res.render("add_post");
 });
 
 app.listen(port, () => console.log(`Sunucu ${port} da başladı`));
