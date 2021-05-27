9d9c09e823ab858094b899f518b4dfc6bee1e90c
Ensar Emir EROL <ensaremir.erol99@gmail.com>
2021-05-27 12:50:18 +0300

    Clean Blog Projesi 3. Bölüm

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
+// include
 const express = require('express');
 const ejs = require('ejs');
 const port = 3000;
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+// Express config
 app.use(express.static("./public"));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("index");
+// MongoDB connection
+mongoose.connect(process.env.HOST_NAME, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Database Connection ERROR:'));
+
+// MongoDB schema and models
+var postSchema = mongoose.Schema(
+    {
+        title: String,
+        detail: String,
+        dateCreated: {type: Date, default: Date.now()}
+    }
+);
+var PostModel = mongoose.model('Post', postSchema);
+
+// GET
+app.get("/", async (req, res) => {
+    var posts = await PostModel.find({})
+    res.render("index", {posts});
 })
 .get("/about", (req, res) => {
     res.render("about");
 })
 .get("/add_post", (req, res) => {
     res.render("add_post");
+});
+
+// POST
+app.post("/post", async (req,res) => {
+    await PostModel.create(req.body);
+    res.redirect("/");
 });
 
 app.listen(port, () => console.log(`Sunucu ${port} da başladı`));
